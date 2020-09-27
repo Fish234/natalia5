3e2a978e2235ebda6f6d668ed5bb88e259038243
Natalia <hourglassbranch25@gmail.com>
2020-09-27 20:13:05 -0300

    history 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,32 @@
 class SessionsController < ApplicationController
 
-   skip_before_action :authorized, only: [:new, :create, :welcome]
+   skip_before_action :authorized, only: [:new, :create, :welcome, :logout]
 
-  def new
-  end
-
-  def create
-   @user = User.find_by(username: params[:username])
-   if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to '/welcome'
-   else
-      redirect_to '/login'
+   def new
    end
-  end
 
-  def login
-  end
+   def create
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to '/welcome'
+      else
+         redirect_to '/login'
+      end
+   end
 
-  def welcome
-  end
+   def login
+   end
 
-  def page_requires_login
-  end
+   def logout
+      session.delete(:user_id)
+      @current_user = nil
+      redirect_to '/welcome'
+   end 
+
+   def welcome
+   end
+
+   def page_requires_login
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#logout'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
 end
